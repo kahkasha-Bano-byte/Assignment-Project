8afa8c533eb25f59914c853fb192aaf383914adc
Kahkasha Bano <kahkasha.bano@olx.in>
2026-05-25 12:32:41 +0530

    add images plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app/index.jsx',
@@ -65,5 +66,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/assets',
+          to: 'assets'
+        }
+      ]
+    })
   ],
 };
